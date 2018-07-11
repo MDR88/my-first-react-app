dd4a15df7086d2dac6eee7f3280adbf22b0722e3
mdroberts74 <mikedroberts88@gmail.com>
2018-07-11 14:58:18 -0500

    My first commit.

---
+++ src/LocationList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class LocationList extends Component
+render() {
+    return ( <
+        article >
+        <
+        div >
+        <
+        h1 > Location List < /h1> <
+        h3 > Student Kennels < /h3> <
+        h4 > Nashville North Location < /h4> <
+        h5 > 500 Puppy Way < /h5> <
+        EmployeeList / >
+        <
+        /div>
+
+        <
+        /article>
+
+
+    );
+
+
+}
+
+
+
+
+
+
+
+
+}
